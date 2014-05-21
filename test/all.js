b667f8c2002c95035926ab0ee6bdf202a3236998
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-21 21:30:36 +1000

    Added combination tests

--- test/all.js
+++ test/all.js
@@ -3,3 +3,4 @@ var compare = require('./helpers/compare');
 
 test('parse an aside into expected output', compare('aside'));
 test('parse a warning section', compare('warning'));
+test('parse a combination section', compare('combination'));
---
+++ test/input/combination.md
@@ -0,0 +1,8 @@
+This is a file containing a simple aside:
+
+A> Line 1
+A> Line 2
+A> Line 3
+D> This is something you can discuss
+
+This content should be outside of the aside section.
